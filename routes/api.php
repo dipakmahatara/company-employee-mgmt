82f57b6823aefbff9d0894b24f5ab11081390ac9
Dipak Mahatara <diplove32928@gmail.com>
2023-05-06 02:27:40 +0545

    Authentication, company and employee routes created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::get('/get-all-companies', [CompanyController::class, 'getAllCompanies']);
+Route::apiResource('companies', CompanyController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+Route::apiResource('employees', EmployeeController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+// Route::apiResource('companies', CompanyController::class)->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
