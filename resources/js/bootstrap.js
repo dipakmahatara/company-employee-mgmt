c83f99e4b87ccd7df724d8673bd1960fc5bd3eb0
Dipak Mahatara <diplove32928@gmail.com>
2023-05-07 00:37:08 +0545

    Route for auth for api

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -36,7 +37,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import {createApp} from 'vue'
 
-import App from './App.vue'
+import App from './layouts/App.vue'
 import router from './route'
 
 const app = createApp(App)
